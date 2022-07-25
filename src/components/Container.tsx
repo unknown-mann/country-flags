5a55d2ab754314889c37eeec286d9c6c612feb58
Duman <duman.abdrakhmanov@outlook.com>
2022-07-26 05:04:51 +0600

    refactoring

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,9 +4,9 @@ export const Container = styled.div`
   width: 100%;
   max-width: 1240px;
   margin: 0 auto;
-  padding: 0 4rem;
+  padding: 0 3rem;
   @media (min-width: 767px) {
-    padding: 0 4rem;
+    padding: 0 2.5rem;
   }
 
   @media (min-width: 1024px) {
